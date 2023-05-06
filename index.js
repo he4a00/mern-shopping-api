8e82a02c297fb9bbad8d1bc86873958793f04aa8
he4a00 <he4a00@github.com>
2023-05-06 22:13:26 +0300

    ds

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://localhost::3000", "https://oganiyo.netlify.app"],
+    origin: "https://oganiyo.netlify.app",
   })
 );
 
